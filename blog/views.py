6c3afff47b8eab819cdeca41fd1c7193dd4fcc0d
webdox <test.webdox@gmail.com>
2019-07-18 18:39:15 +0530

    First Commit of Amit's project

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from blog.models import Post
+
+# Create your views here.
+def post_list(request):
+	post = Post.objects.order_by('-created_date')
+	return render(request, 'post_list.html', {'post' : post})
